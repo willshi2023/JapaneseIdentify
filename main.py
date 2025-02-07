05df536ad35979fff4248a2b788a9e45110460c2
will shi <1623299667@qq.com>
2025-02-07 16:46:34 +0900

    增加时间统计

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import speech_recognition as sr
-
+import time
 from pydub import AudioSegment
 
-filename = r'C:\Users\shiji\PycharmProjects\JapaneseIdentify\resource\2025年2月7日 9_41_15.m4a'
+
+filename = r'C:\Users\shiji\PycharmProjects\JapaneseIdentify\resource\2024年9月10日 9_20_24.m4a'
+start = time.perf_counter()
 # 转换m4a为wav
 sound = AudioSegment.from_file(filename, format="m4a")
 filename = filename + '.wav'
 sound.export(filename, format="wav")
+end = time.perf_counter()
+print('录音文件格式转换耗费的时间：{:.2f}秒'.format(end - start))
 
+start = time.perf_counter()
 recognizer = sr.Recognizer()
 with sr.AudioFile(filename) as source:
     audio_data = recognizer.record(source)
     text = recognizer.recognize_google(audio_data, language="ja-JP")
 print(text)
+end = time.perf_counter()
+print('录音文件识别耗费的时间：{:.2f}秒'.format(end - start))
